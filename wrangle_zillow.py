6c73930dec2148dfda4c96ae02a1c925aae3623d
Bibek Mainali <bibek.mainali20@gmail.com>
2020-10-21 22:19:24 -0500

    Updated

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -42,7 +42,16 @@ def clean_zillow(df):
     df.drop(columns = ['finishedsquarefeet12','propertylandusetypeid', 'calculatedbathnbr'], inplace = True)
     return df
 
-
+def check_outliers(df, cols_to_check):
+    '''takes in a dataframe, list of columns to check outliers for
+    and returns a df with the outliers removed'''
+    
+    for col in cols_to_check:
+        iqr = df[col].quantile(0.75) - df[col].quantile(0.25)
+        upper_bound = df[col].quantile(0.75) +  1.5 * iqr
+        lower_bound = df[col].quantile(0.25) - 1.5 * iqr
+        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
+    return df  
 
 
 
@@ -52,6 +61,49 @@ def split_zillow(df):
     train, validate = train_test_split(train_validate, test_size = .2, random_state = 123)
     
     return train, test, validate
+
+
+
+def scale_data(train, test, validate, scaler):
+    '''Takes in a train, test, validate dataframe, scales is based on the type of scaled passed and returns the 
+    scaled version of train, test, and validate'''
+
+    scaler = scaler
+    scaler = scaler.fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns = train.columns, index = train.index)
+
+    validate_scaled = pd.DataFrame(scaler.transform(validate), columns = validate.columns, index = validate.index)
+
+
+    test_scaled = pd.DataFrame(scaler.transform(test), columns = test.columns, index = test.index)
+    
+    return train_scaled, test_scaled, validate_scaled
+    
+
+
+### Creating a dataframe with the name of the columns, number of missing rows in that columns, and pct of missing rows
+
+def get_missing_col_summary(df):
+    summary_df = pd.DataFrame( columns = ['Number of rows missing', "pct of rows missing"], index = [df.columns])
+    summary_df['Number of rows missing'] = list(df.isnull().sum())
+    summary_df['pct of rows missing'] = (summary_df['Number of rows missing']/len(df)) * 100
+    return summary_df
+
+### Write a function that takes in a dataframe and returns a dataframe with 3 columns: the number of columns missing, percent of columns missing, and number of rows with n columns missing. Run the function and document takeaways from this on how you want to handle missing values.
+def get_missing_row_summary(df):
+
+    cols_summary = pd.DataFrame(df.isnull().sum(axis = 1).value_counts())
+    cols_summary = cols_summary.reset_index()
+    cols_summary = cols_summary.rename(columns = {'index': 'num_cols_missing'})
+
+    cols_summary['pct_cols_missing'] = (cols_summary.num_cols_missing/len(df.columns)) * 100
+
+    cols_summary = cols_summary.rename(columns = {0:'num_rows'})
+
+    return cols_summary
+
+
+
 
 
 
